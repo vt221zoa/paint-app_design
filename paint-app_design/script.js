f7cb7ba4aa9f001120c41fe269767af37220c61f
anastasiia <vt221_laa@student.ztu.edu.ua>
2024-06-03 15:01:40 +0300

    Added 2 shapes, opasity and mod user pannel

--- paint-app_design/script.js
+++ paint-app_design/script.js
@@ -15,6 +15,8 @@ document.addEventListener('DOMContentLoaded',   () => {
     const drawCircleButton = document.getElementById('drawCircle');
     const drawLineButton = document.getElementById('drawLine');
     const drawTriangleButton = document.getElementById('drawTriangle');
+    const drawHeartButton = document.getElementById('drawHeart');
+    const drawStarButton = document.getElementById('drawStar');
     const fillCheckbox = document.getElementById('fillCheckbox');
 
     const brushToolButton = document.getElementById('brushTool');
@@ -56,7 +58,7 @@ document.addEventListener('DOMContentLoaded',   () => {
     function startPosition(e) {
         painting = true;
         const rect = canvas.getBoundingClientRect();
-        if (['rectangle', 'circle', 'line', 'triangle'].includes(activeShape)) {
+        if (['rectangle', 'circle', 'line', 'triangle', 'heart', 'star'].includes(activeShape)) {
             canvas.startX = e.clientX - rect.left;
             canvas.startY = e.clientY - rect.top;
         } else {
@@ -67,7 +69,7 @@ document.addEventListener('DOMContentLoaded',   () => {
 
     function endPosition(e) {
         if (painting) {
-            if (['rectangle', 'circle', 'line', 'triangle'].includes(activeShape)) {
+            if (['rectangle', 'circle', 'line', 'triangle', 'heart', 'star'].includes(activeShape)) {
                 drawShape(e);
             }
             undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
@@ -113,17 +115,19 @@ document.addEventListener('DOMContentLoaded',   () => {
     }
 
     class Brush extends Tool {
-        constructor(ctx, colorPicker, sizePicker) {
+        constructor(ctx, colorPicker, sizePicker, opacitySlider) {
             super();
             this.ctx = ctx;
             this.colorPicker = colorPicker;
             this.sizePicker = sizePicker;
+            this.opacitySlider = opacitySlider;
         }
 
         draw(e) {
             this.ctx.lineWidth = this.sizePicker.value;
             this.ctx.lineCap = 'round';
             this.ctx.strokeStyle = this.colorPicker.value;
+            this.ctx.globalAlpha = this.opacitySlider.value;
 
             const [x, y] = getCursorPosition(e);
             this.ctx.lineTo(x, y);
@@ -132,6 +136,7 @@ document.addEventListener('DOMContentLoaded',   () => {
             this.ctx.moveTo(x, y);
         }
     }
+
 
     class Eraser extends Tool {
         constructor(ctx, sizePicker) {
@@ -154,12 +159,13 @@ document.addEventListener('DOMContentLoaded',   () => {
     }
 
     class Spray extends Tool {
-        constructor(ctx, palette, colorIndex, sizePicker) {
+        constructor(ctx, palette, colorIndex, sizePicker, opacitySlider) {
             super();
             this.ctx = ctx;
             this.palette = palette;
             this.colorIndex = colorIndex;
             this.sizePicker = sizePicker;
+            this.opacitySlider = opacitySlider;
         }
 
         draw(e) {
@@ -168,16 +174,18 @@ document.addEventListener('DOMContentLoaded',   () => {
             for (let i = 0; i < 10; i++) {
                 const offsetX = Math.random() * this.sizePicker.value - this.sizePicker.value / 2;
                 const offsetY = Math.random() * this.sizePicker.value - this.sizePicker.value / 2;
+                this.ctx.globalAlpha = Math.random() * this.opacitySlider.value; // Використовуємо opacitySlider
                 this.ctx.fillRect(x + offsetX, y + offsetY, 1, 1);
             }
         }
     }
     class Marker extends Tool {
-        constructor(ctx, colorPicker, sizePicker) {
+        constructor(ctx, colorPicker, sizePicker, opacitySlider) {
             super();
             this.ctx = ctx;
             this.colorPicker = colorPicker;
             this.sizePicker = sizePicker;
+            this.opacitySlider = opacitySlider;
             this.initialAlpha = 0.5;
         }
 
@@ -185,7 +193,7 @@ document.addEventListener('DOMContentLoaded',   () => {
             this.ctx.lineWidth = this.sizePicker.value;
             this.ctx.lineCap = 'round';
             this.ctx.strokeStyle = this.colorPicker.value;
-            this.ctx.globalAlpha = this.initialAlpha;
+            this.ctx.globalAlpha = this.initialAlpha * this.opacitySlider.value; // Використовуємо opacitySlider
 
             const [x, y] = getCursorPosition(e);
             this.ctx.lineTo(x, y);
@@ -202,11 +210,12 @@ document.addEventListener('DOMContentLoaded',   () => {
     }
 
     class OilBrush extends Tool {
-        constructor(ctx, colorPicker, sizePicker) {
+        constructor(ctx, colorPicker, sizePicker, opacitySlider) {
             super();
             this.ctx = ctx;
             this.colorPicker = colorPicker;
             this.sizePicker = sizePicker;
+            this.opacitySlider = opacitySlider;
             this.density = 5;
             this.initialAlpha = 0.2;
         }
@@ -215,7 +224,7 @@ document.addEventListener('DOMContentLoaded',   () => {
             this.ctx.lineWidth = this.sizePicker.value;
             this.ctx.lineCap = 'round';
             this.ctx.strokeStyle = this.colorPicker.value;
-            this.ctx.globalAlpha = this.initialAlpha;
+            this.ctx.globalAlpha = this.initialAlpha * this.opacitySlider.value; // Використовуємо opacitySlider
 
             const [x, y] = getCursorPosition(e);
             for (let i = 0; i < this.density; i++) {
@@ -234,24 +243,33 @@ document.addEventListener('DOMContentLoaded',   () => {
     }
 
     class ToolFactory {
-        static createTool(type, ctx, colorPicker, sizePicker, palette, colorIndex) {
+        static createTool(type, ctx, colorPicker, sizePicker, palette, colorIndex, opacitySlider) {
             switch (type) {
                 case 'brush':
-                    return new Brush(ctx, colorPicker, sizePicker);
+                    return new Brush(ctx, colorPicker, sizePicker, opacitySlider);
                 case 'eraser':
-                    return new Eraser(ctx, sizePicker);
+                    return new Eraser(ctx, sizePicker, opacitySlider);
                 case 'spray':
-                    return new Spray(ctx, palette, colorIndex, sizePicker);
+                    return new Spray(ctx, palette, colorIndex, sizePicker, opacitySlider);
                 case 'oilBrush':
-                    return new OilBrush(ctx, colorPicker, sizePicker);
+                    return new OilBrush(ctx, colorPicker, sizePicker, opacitySlider);
                 case 'marker':
-                    return new Marker(ctx, colorPicker, sizePicker);
+                    return new Marker(ctx, colorPicker, sizePicker, opacitySlider);
                 default:
                     throw new Error("Unknown tool type.");
             }
         }
     }
     let currentTool = ToolFactory.createTool('brush', ctx, colorPicker, sizePicker, palette, colorIndex);
+
+    const opacitySlider = document.getElementById('opacitySlider');
+
+    opacitySlider.addEventListener('input', () => {
+        if (currentTool && currentTool.hasOwnProperty('opacitySlider')) {
+            currentTool.opacitySlider = opacitySlider;
+        }
+    });
+
 
     function drawShape(e) {
         const rect = canvas.getBoundingClientRect();
@@ -281,10 +299,96 @@ document.addEventListener('DOMContentLoaded',   () => {
             case 'triangle':
                 drawTriangle(startX, startY, mouseX, mouseY);
                 break;
+            case 'star':
+                drawStar(startX, startY, mouseX, mouseY);
+                break;
+            case 'heart':
+                drawHeart(startX, startY, mouseX, mouseY);
+                break;
             default:
                 break;
         }
     }
+
+    function drawStar(startX, startY, endX, endY) {
+        var dx = endX - startX;
+        var dy = endY - startY;
+        var angle = Math.atan2(dy, dx);
+        var starRadius = Math.sqrt(dx*dx + dy*dy) / 2; // Радіус зірки (можна налаштувати)
+
+        ctx.lineCap = "round";
+        ctx.lineWidth = 2; // Товщина зірки (можна налаштувати)
+        ctx.beginPath();
+        ctx.moveTo(startX + Math.cos(angle) * starRadius, startY + Math.sin(angle) * starRadius);
+
+        for (var i = 0; i < 5; i++) {
+            ctx.lineTo(startX + Math.cos(angle + i * Math.PI * 0.4) * starRadius,
+                startY + Math.sin(angle + i * Math.PI * 0.4) * starRadius);
+            ctx.lineTo(startX + Math.cos(angle + (i + 0.5) * Math.PI * 0.4) * starRadius * 0.5,
+                startY + Math.sin(angle + (i + 0.5) * Math.PI * 0.4) * starRadius * 0.5);
+        }
+
+        ctx.closePath();
+        if (fillCheckbox.checked) {
+            ctx.fill(); // Заливаємо серце
+        } else {
+            ctx.stroke(); // Малюємо контур
+        }
+
+        ctx.restore();
+    }
+
+
+    function drawHeart(startX, startY, endX, endY) {
+        var x = startX;
+        var y = startY;
+        var width = Math.abs(endX - startX);
+        var height = Math.abs(endY - startY);
+
+        ctx.save();
+        ctx.beginPath();
+        var topCurveHeight = height * 0.3;
+        ctx.moveTo(x, y + topCurveHeight);
+        // top left curve
+        ctx.bezierCurveTo(
+            x, y,
+            x - width / 2, y,
+            x - width / 2, y + topCurveHeight
+        );
+
+        // bottom left curve
+        ctx.bezierCurveTo(
+            x - width / 2, y + (height + topCurveHeight) / 2,
+            x, y + (height + topCurveHeight) / 2,
+            x, y + height
+        );
+
+        // bottom right curve
+        ctx.bezierCurveTo(
+            x, y + (height + topCurveHeight) / 2,
+            x + width / 2, y + (height + topCurveHeight) / 2,
+            x + width / 2, y + topCurveHeight
+        );
+
+        // top right curve
+        ctx.bezierCurveTo(
+            x + width / 2, y,
+            x, y,
+            x, y + topCurveHeight
+        );
+
+        ctx.closePath();
+
+        if (fillCheckbox.checked) {
+            ctx.fill(); // Заливаємо серце
+        } else {
+            ctx.stroke(); // Малюємо контур
+        }
+
+        ctx.restore();
+    }
+
+
 
     function drawRectangle(startX, startY, endX, endY) {
         if (fillCheckbox.checked) {
@@ -372,10 +476,10 @@ document.addEventListener('DOMContentLoaded',   () => {
     function setActiveTool(toolType, button) {
         activeShape = null;
         tool = toolType;
-        currentTool = ToolFactory.createTool(toolType, ctx, colorPicker, sizePicker, palette, colorIndex);
+        currentTool = ToolFactory.createTool(toolType, ctx, colorPicker, sizePicker, palette, colorIndex, opacitySlider);
         document.querySelectorAll('.btn.tool').forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
-        updateObjectInfo(tool,sizePicker);
+        updateObjectInfo(tool, sizePicker);
     }
 
     function setActiveShape(shapeType, button) {
@@ -390,6 +494,14 @@ document.addEventListener('DOMContentLoaded',   () => {
         const rect = canvas.getBoundingClientRect();
         return [e.clientX - rect.left, e.clientY - rect.top];
     }
+
+    const shapeSelector = document.getElementById('shapeSelector');
+
+    shapeSelector.addEventListener('change', function() {
+        const selectedShape = shapeSelector.value;
+        setActiveShape(selectedShape);
+    });
+
 
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', endPosition);
@@ -411,6 +523,9 @@ document.addEventListener('DOMContentLoaded',   () => {
     drawCircleButton.addEventListener('click', () => setActiveShape('circle', drawCircleButton));
     drawLineButton.addEventListener('click', () => setActiveShape('line', drawLineButton));
     drawTriangleButton.addEventListener('click', () => setActiveShape('triangle', drawTriangleButton));
+    drawHeartButton.addEventListener('click', () => setActiveShape('heart', drawHeartButton));
+    drawStarButton.addEventListener('click', () => setActiveShape('star', drawStarButton));
+
 
     resizeCanvasButton.addEventListener('click', () => {
         const width = parseInt(canvasWidthInput.value);
